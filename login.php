bca5aaa817d9fb10f4f1d96128fd6aa34655397e
yama0831 <naki.winning.0831@gmail.com>
2013-09-04 00:25:06 +0900

    ログイン追加

---
+++ login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>login</title>
+	<link href="./css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	ログイン<br>
+	<form action="" method="POST">
+		<table>
+			<tbody>
+				<tr>
+					<th>
+						<label for="user_name">ユーザーID</label>
+					</th>
+					<td>
+						<input type="text" name="user_name" id="user_name"><br>
+					</td>
+				</tr>
+				<tr->
+					<th>
+						<label for="pass">パスワード</label>
+					</th>
+					<td>
+						<input type="password" name="pass" id="pass">
+					</td>
+				</tr->
+			</tbody>
+		</table>
+	</form>
+</body>
+</html>
