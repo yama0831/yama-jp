b0502a53d93d12bb4f80bf71500ced83591ac031
yama0831 <naki.winning.0831@gmail.com>
2013-09-04 18:27:25 +0900

    templateの追加

---
+++ template.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>template</title>
+	<link href="./css/template.css" rel="stylesheet">
+	<link href="./css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<div class="nav_wrapper">
+			<div class="nav_container">
+				<a class="logo" title="yama-jp.net" href="/login.php">
+					<h1>yama-jp.net</h1>
+				</a>
+			</div>
+		</div>
+	</header>
+	<!--<?php $Cotent ?>-->ssssssss
+	<footer>aaaaaaaaa</footer>
+</body>
+</html>
