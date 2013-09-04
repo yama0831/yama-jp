b03bcc0fe97c4e5b32e985488c9a55f199dcf566
yama0831 <naki.winning.0831@gmail.com>
2013-09-04 16:35:16 +0900

    ログイン画面修正

--- login.php
+++ login.php
@@ -3,31 +3,41 @@
 <head>
 	<meta charset="UTF-8">
 	<title>login</title>
-	<link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/bootstrap.css" rel="stylesheet">
+	<link href="./css/login.css" rel="stylesheet">
 </head>
-<body>
-	ログイン<br>
-	<form action="" method="POST">
-		<table>
-			<tbody>
-				<tr>
-					<th>
-						<label for="user_name">ユーザーID</label>
-					</th>
-					<td>
-						<input type="text" name="user_name" id="user_name"><br>
-					</td>
-				</tr>
-				<tr->
-					<th>
-						<label for="pass">パスワード</label>
-					</th>
-					<td>
-						<input type="password" name="pass" id="pass">
-					</td>
-				</tr->
-			</tbody>
-		</table>
-	</form>
+<body id="public" class="login" data-twttr-rendered="true">
+	<div id="header">
+		<div id="hero-container">
+			<div id="hero" class="pad">
+				<h1>Log In</h1>
+			</div>
+		</div>
+	</div>
+	<div id="wrapper">
+		<div id="shell" class="container">
+			<div id="content" class="pad">
+				<form action="" method="POST">
+					<p>
+						<span class="field big">
+							<label for="user_name">ユーザーID</label>
+							<input type="text" name="user_name" id="user_name" tabindex="1">
+						</span>
+					</p>
+					<p>
+						<span class="field password big">
+							<label for="pass">パスワード</label>
+							<input type="password" name="pass" id="pass" tabindex="2">
+						</span>
+					</p>
+					<div id="form_submit">
+						<input type="submit" value="  login  " tabindex="3" class="submit link green">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+	</div>
 </body>
 </html>
