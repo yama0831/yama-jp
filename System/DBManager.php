cb4896e3b3d3cdeed41d35f2bd17316bfe78d563
yama0831 <naki.winning.0831@gmail.com>
2013-09-12 01:15:10 +0900

    DB周り動作検証

--- System/DBManager.php
+++ System/DBManager.php
@@ -1,8 +1,7 @@
 <?php
-
 	function getDb()
 	{
-		$dsn = 'mysql:dbname=seigyo_yama; host=mysql474.db.sakura.ne.jp';
+		$dsn = 'mysql:dbname=seigyo_yama;host=mysql474.db.sakura.ne.jp';
 		$user = 'seigyo';
 		$pass = 'seigyo0831';
 
@@ -10,7 +9,6 @@
 		{
 			// DBへ接続を確立
 			$db = new PDO($dsn, $user, $pass);
-			//print '接続に成功しました。';
 
 			// 文字コードを明示
 			$db->exec('SET NAMES utf8');
@@ -19,7 +17,5 @@
 		{
 			die("接続エラー: {$e->getMessage()}");
 		}
-
 		return $db;
 	}
-?>
--- System/DBTest.php
+++ System/DBTest.php
@@ -4,13 +4,12 @@
 	// DBオブジェクトの取得
 	$db = getDb();
 
-	// ★DBからユーザーID、パスワードを取得
+	// DBからユーザーID、パスワードを取得
 	$result =  $db->query('SELECT * FROM M_USER');
-	//$result =  $db->query('SELECT * FROM M_USER WHERE user_name = \'' . $_POST['user_name'] . '\'' );
-	//$row = $result->fetch(PDO::FETCH_ASSOC);
 
-	while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-		print($result['name']);
-		print($result['password'].'<br>');
+	// 取得データを展開
+	while($row = $result->fetch(PDO::FETCH_ASSOC)){
+		print($row['name']);
+		print($row['password'].'<br>');
 	}
 ?>
